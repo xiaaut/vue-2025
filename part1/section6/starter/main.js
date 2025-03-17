a39f5cd8cdc1397096d0e048589f81485164c9a0
xia <836807919@qq.com>
2025-03-18 00:00:41 +0800

    列表渲染

--- part1/section6/starter/main.js
+++ part1/section6/starter/main.js
@@ -1,1 +1,25 @@
 // ["Vue", "React", "Nuxt", "NextJS", "Node", "NestJS", "React Native", "Electron", "Rust"]
+const { createApp } = Vue;
+
+createApp({
+    data () {
+        return {
+            todoArray: ["Vue", "React", "Nuxt", "NextJS", "Node", "NestJS", "React Native", "Electron", "Rust"],
+            inputText: ""
+        }
+    },
+    methods:{
+        addTodo() {
+            if( !this.inputText.trim().length) {
+                alert("Please enter a valid todo");
+                this.inputText = "";
+                return
+            }
+            this.todoArray.push(this.inputText);
+            this.inputText = "";
+        },
+        removeTodo(removeIndex) {
+            this.todoArray = this.todoArray.filter((_todo, index) => index !== removeIndex)
+        }
+    }
+}).mount("#app");
